76ee5709f232d215a4c6ceef7707b3716fd725d5
Koshi <rakukoshi@gmail.com>
2026-05-07 15:58:41 +0530

    HA_Sim file edited

--- NanoProcessor/Simulation/HA_Sim.vhd
+++ NanoProcessor/Simulation/HA_Sim.vhd
@@ -6,28 +6,35 @@ entity HA_Sim is
 end HA_Sim;
 
 architecture Behavioral of HA_Sim is
+
 COMPONENT HA
     port(A,B:in std_logic;
         S,C:out std_logic);
 END COMPONENT;
+
 signal a,b,c,s :std_logic;
+
 begin
 UUT: HA PORT MAP(
     A=>a,
     B=>b,
     C=>c,
     S=>s);
+
  process
  begin
-    a<='0';
-    b<='0';
+    a <= '0';
+    b <= '0';
     wait for 100ns;
-    b<='1';
+
+    b <= '1';
     wait for 100ns;
-    a<='1';
-    b<='0';
+
+    a <= '1';
+    b <= '0';
     wait for 100ns;
-    b<='1';
+
+    b <= '1';
     wait;
  end process;
 

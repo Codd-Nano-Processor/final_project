d057b9eb632330d9e5207c578d62edd5a5a66b17
Lasith Eshan <168047758+lasitheshanJR@users.noreply.github.com>
2026-05-08 19:37:59 +0530

    Update TB_Nanoprocessor.vhd

--- NanoProcessor_Extended/NanoProcessor_Srcs/sim_1/TB_Nanoprocessor.vhd
+++ NanoProcessor_Extended/NanoProcessor_Srcs/sim_1/TB_Nanoprocessor.vhd
@@ -1,52 +1,37 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
-entity TB_Nanoprocessor is
---  Port ( );
-end TB_Nanoprocessor;
+entity TB_NanoProcessor is
+end TB_NanoProcessor;
 
-architecture Behavioral of TB_Nanoprocessor is
+architecture Behavioral of TB_NanoProcessor is
+    component NanoProcessor
+        Port ( clk : in STD_LOGIC;
+               reset : in STD_LOGIC;
+               reg : out STD_LOGIC_VECTOR (3 downto 0);
+               zero : out STD_LOGIC;
+               overflow : out STD_LOGIC;
+               display : out STD_LOGIC_VECTOR (6 downto 0);
+               anode : out STD_LOGIC_VECTOR (3 downto 0);
+               carry : out STD_LOGIC;
+               var0, var1 : out STD_LOGIC_VECTOR (3 downto 0));
+    end component;
 
-component NanoProcessor
-    Port (clk : in STD_LOGIC;
-          reset : in STD_LOGIC; 
-          reg : out STD_LOGIC_VECTOR (3 downto 0);
-          display : out STD_LOGIC_VECTOR (6 downto 0);
-          zero : out STD_LOGIC;
-          overflow : out STD_LOGIC;
-          anode : out STD_LOGIC_VECTOR (3 downto 0);
-          carry: out STD_LOGIC   );
-end component;
-Signal clk,reset,zero,overflow,carry : STD_LOGIC;
-signal reg,anode : STD_LOGIC_VECTOR (3 downto 0);
-signal display : STD_LOGIC_VECTOR (6 downto 0);
+    signal clk, reset, zero, overflow, carry : std_logic := '0';
+    signal reg, anode, var0, var1 : std_logic_vector(3 downto 0);
+    signal display : std_logic_vector(6 downto 0);
 
 begin
-UUT : NanoProcessor
-    Port Map (
-        clk => clk,
-        reset => reset,
-        reg => reg,
-        zero => zero,
-        overflow => overflow,
-        carry => carry,
-        display => display,
-        anode => anode
-    );
+    UUT: NanoProcessor port map (clk, reset, reg, zero, overflow, display, anode, carry, var0, var1);
 
-process
-begin
-    clk <= '0';
-    wait for 50 ns;
-    clk <= '1';
-    wait for 50 ns;
-end process;
+    clk <= not clk after 50 ns;
 
-process
-begin
-    reset <= '1';
-    wait for 100ns;
-    reset <= '0';
-    wait;
-end process;
+    process
+    begin
+        reset <= '1'; wait for 100 ns;
+        reset <= '0';
+        -- Let the processor run for several clock cycles to execute ROM instructions
+        wait for 2000 ns;
+        wait;
+    end process;
 end Behavioral;

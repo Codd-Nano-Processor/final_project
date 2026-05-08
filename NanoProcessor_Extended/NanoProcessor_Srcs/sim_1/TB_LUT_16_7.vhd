669448ef7e0688359331f398d2ca2a87d1807f39
Lasith Eshan <168047758+lasitheshanJR@users.noreply.github.com>
2026-05-08 19:34:55 +0530

    Update TB_LUT_16_7.vhd

--- NanoProcessor_Extended/NanoProcessor_Srcs/sim_1/TB_LUT_16_7.vhd
+++ NanoProcessor_Extended/NanoProcessor_Srcs/sim_1/TB_LUT_16_7.vhd
@@ -5,61 +5,18 @@ entity TB_LUT_16_7 is
 end TB_LUT_16_7;
 
 architecture Behavioral of TB_LUT_16_7 is
-
-component LUT_16_7
-    Port (
-        address : in  STD_LOGIC_VECTOR (3 downto 0);
-        data    : out STD_LOGIC_VECTOR (6 downto 0)
-    );
-end component;
-
-signal address : STD_LOGIC_VECTOR (3 downto 0);
-signal data    : STD_LOGIC_VECTOR (6 downto 0);
-
+    component LUT_16_7
+        Port ( address : in STD_LOGIC_VECTOR (3 downto 0); data : out STD_LOGIC_VECTOR (6 downto 0));
+    end component;
+    signal addr : std_logic_vector(3 downto 0);
+    signal data : std_logic_vector(6 downto 0);
 begin
-
-UUT : LUT_16_7
-    port map(
-        address => address,
-        data => data
-    );
-
--- 240540U = 11 1010 1011 1001 1100
--- 240558G = 11 1010 1011 1010 1110
--- 240574C = 11 1010 1011 1011 1110
--- 240589C = 11 1010 1011 1100 1101
-
-process
-begin
-
-    -- Index number based values
-    address <= "1010";  -- A
-    wait for 100 ns;
-
-    address <= "1011";  -- B
-    wait for 100 ns;
-
-    address <= "1001";  -- 9
-    wait for 100 ns;
-
-    address <= "1100";  -- C
-    wait for 100 ns;
-
-    address <= "1110";  -- E
-    wait for 100 ns;
-
-    address <= "1101";  -- D
-    wait for 100 ns;
-
-    -- Extra checking
-    address <= "0000";  -- 0
-    wait for 100 ns;
-
-    address <= "1111";  -- F
-    wait for 100 ns;
-
-    wait;
-
-end process;
-
+    UUT: LUT_16_7 port map (addr, data);
+    process
+    begin
+        addr <= "1001"; wait for 100 ns; -- Display '9' (from 540U)
+        addr <= "1110"; wait for 100 ns; -- Display 'E' (from 558G)
+        addr <= "1101"; wait for 100 ns; -- Display 'd' (from 589C)
+        wait;
+    end process;
 end Behavioral;

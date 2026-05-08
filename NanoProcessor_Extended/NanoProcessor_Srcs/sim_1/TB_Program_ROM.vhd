b8ff0c6448ed78f4b505ac107f16339950ce104b
Lasith Eshan <168047758+lasitheshanJR@users.noreply.github.com>
2026-05-08 19:37:25 +0530

    Update TB_Program_ROM.vhd

--- NanoProcessor_Extended/NanoProcessor_Srcs/sim_1/TB_Program_ROM.vhd
+++ NanoProcessor_Extended/NanoProcessor_Srcs/sim_1/TB_Program_ROM.vhd
@@ -5,54 +5,23 @@ entity TB_Program_ROM is
 end TB_Program_ROM;
 
 architecture Behavioral of TB_Program_ROM is
+    component Program_ROM
+        Port ( address : in STD_LOGIC_VECTOR (2 downto 0);
+               instruction_code : out STD_LOGIC_VECTOR (12 downto 0));
+    end component;
 
-component Program_ROM
-    Port (
-        address          : in  STD_LOGIC_VECTOR (2 downto 0);
-        instruction_code : out STD_LOGIC_VECTOR (11 downto 0)
-    );
-end component;
-
-signal address          : STD_LOGIC_VECTOR (2 downto 0);
-signal instruction_code : STD_LOGIC_VECTOR (11 downto 0);
+    signal address : std_logic_vector(2 downto 0);
+    signal instruction_code : std_logic_vector(12 downto 0);
 
 begin
+    UUT: Program_ROM port map (address => address, instruction_code => instruction_code);
 
-UUT : Program_ROM
-    port map(
-        address => address,
-        instruction_code => instruction_code
-    );
-
-process
-begin
-
-    address <= "000";   -- ROM address 0
-    wait for 100 ns;
-
-    address <= "001";   -- ROM address 1
-    wait for 100 ns;
-
-    address <= "010";   -- ROM address 2
-    wait for 100 ns;
-
-    address <= "011";   -- ROM address 3
-    wait for 100 ns;
-
-    address <= "100";   -- ROM address 4
-    wait for 100 ns;
-
-    address <= "101";   -- ROM address 5
-    wait for 100 ns;
-
-    address <= "110";   -- ROM address 6
-    wait for 100 ns;
-
-    address <= "111";   -- ROM address 7
-    wait for 100 ns;
-
-    wait;
-
-end process;
-
+    process
+    begin
+        address <= "000"; wait for 100 ns;
+        address <= "001"; wait for 100 ns;
+        address <= "010"; wait for 100 ns;
+        address <= "111"; wait for 100 ns;
+        wait;
+    end process;
 end Behavioral;
